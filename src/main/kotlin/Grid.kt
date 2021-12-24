c48d9299eacf0bbdb0eb54696d4ee749696e0692
Fawzi Bedidi <fawzi.bedidi@iqair.com>
2021-12-24 15:23:09 +0800

    Done

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -1,9 +1,11 @@
 import org.jetbrains.kotlinx.multik.api.mk
 import org.jetbrains.kotlinx.multik.api.zeros
+import org.jetbrains.kotlinx.multik.ndarray.data.D2Array
 import org.jetbrains.kotlinx.multik.ndarray.data.get
 import org.jetbrains.kotlinx.multik.ndarray.data.set
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
+import org.openrndr.extra.noise.random
 import org.openrndr.math.Vector2
 import kotlin.math.floor
 import kotlin.math.roundToInt
@@ -11,11 +13,31 @@ import kotlin.math.roundToInt
 class Grid(
     width: Int = WINDOW_WIDTH,
     height: Int = WINDOW_HEIGHT,
-    private val squareSize: Double = SQUARE_SIZE
+    private val squareSize: Double = SQUARE_SIZE,
 ) {
     private val columns: Int = floor(width / squareSize).roundToInt()
     private val rows: Int = floor(height / squareSize).roundToInt()
     private val matrix = mk.zeros<Int>(columns, rows)
+
+    fun getMatrix(): D2Array<Int> {
+        return matrix
+    }
+
+    fun clearGrid() {
+        for (x in 0 until columns) {
+            for (y in 0 until rows) {
+                matrix[x, y] = 0
+            }
+        }
+    }
+
+    fun randomizeGrid() {
+        for (x in 0 until columns) {
+            for (y in 0 until rows) {
+                matrix[x, y] = random(0.0, 1.1).toInt()
+            }
+        }
+    }
 
     fun draw(drawer: Drawer) {
         for (x in 0 until columns) {
--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -1,5 +1,69 @@
+import org.jetbrains.kotlinx.multik.ndarray.data.D2Array
+import org.jetbrains.kotlinx.multik.ndarray.data.get
+import org.jetbrains.kotlinx.multik.ndarray.data.set
+import org.openrndr.KEY_SPACEBAR
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+
+fun countNeighbors(x: Int, y: Int, matrix: D2Array<Int>): Int {
+    var neighbors = 0
+    val columns = matrix.shape[0]
+    val rows = matrix.shape[1]
+    // TOP ROW
+    if (y > 0) {
+        if (x > 0 && matrix[x - 1, y - 1] == 1) {
+            neighbors += 1
+        }
+        if (matrix[x, y - 1] == 1) {
+            neighbors += 1
+        }
+        if (x < columns - 1 && matrix[x + 1, y - 1] == 1) {
+            neighbors += 1
+        }
+    }
+
+    // MIDDLE ROW
+    if (x > 0 && matrix[x - 1, y] == 1) {
+        neighbors += 1
+    }
+    if (x < columns - 1 && matrix[x + 1, y] == 1) {
+        neighbors += 1
+    }
+
+    // BOTTOM ROW
+    if (y < rows - 1) {
+        if (x > 0 && matrix[x - 1, y + 1] == 1) {
+            neighbors += 1
+        }
+        if (matrix[x, y + 1] == 1) {
+            neighbors += 1
+        }
+        if (x < columns - 1 && matrix[x + 1, y + 1] == 1) {
+            neighbors += 1
+        }
+    }
+
+    return neighbors
+}
+
+fun processGameOfLife(matrix: D2Array<Int>) {
+    for (x in 0 until matrix.shape[0]) {
+        for (y in 0 until matrix.shape[1]) {
+            val neighbors = countNeighbors(x, y, matrix)
+            if (matrix[x, y] == 0) {
+                if (neighbors == 3) {
+                    matrix[x, y] = 1
+                }
+            } else {
+                if (neighbors <= 1) {
+                    matrix[x, y] = 0
+                } else if (neighbors >= 4) {
+                    matrix[x, y] = 0
+                }
+            }
+        }
+    }
+}
 
 fun main() = application {
     configure {
@@ -9,12 +73,26 @@ fun main() = application {
 
     program {
         val grid = Grid(WINDOW_WIDTH, WINDOW_HEIGHT, SQUARE_SIZE)
+        var isRunning = false
+        keyboard.keyUp.listen {
+            if (it.key == KEY_SPACEBAR) {
+                isRunning = !isRunning
+            } else if (it.name == "c" || it.name == "C") {
+                grid.clearGrid()
+            } else if (it.name == "r" || it.name == "R") {
+                grid.randomizeGrid()
+            } else {
+                println("Pressed ${it.name} ${it.key} ")
+            }
+        }
         mouse.buttonUp.listen {
             grid.clickTrigger(it.position)
         }
         extend {
             drawer.clear(ColorRGBa.PINK)
             grid.draw(drawer)
+            if (grid.getMatrix().size > 0 && isRunning)
+                processGameOfLife(grid.getMatrix())
         }
     }
 }
